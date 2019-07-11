8c11084a41607dca1cb5747dc7be2765437b9aa0
Jessica <jessica.homet@gmail.com>
2019-07-11 14:35:46 -0700

    add dots for bikes to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,26 @@
-// json source: https://catalog.data.gov/dataset/042242c7-443e-4c2b-93ed-7dcd2bd6d8f4/resource/77db88b7-20ad-400b-aebb-3400e3042773
+// geojson seattle outline source: https://catalog.data.gov/dataset/042242c7-443e-4c2b-93ed-7dcd2bd6d8f4/resource/77db88b7-20ad-400b-aebb-3400e3042773
 
 import React, {Component} from 'react';
 import * as d3 from "d3";
+// commented out while in test mode
 import axios from "axios";
 import seattleJson from "../data/seattleJson"
+import jumpApiCache from "../data/jumpApiCache"
+
 
 class Map extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {dots: this.geoJsonify(jumpApiCache)};
+  }
 
   componentDidMount() {
     this.drawMap();
   }
 
-  drawMap() {
-    console.log(seattleJson)
-      
-    var width = 700;
-    var height = 580;
+  drawMap() {      
+    var width = 400;
+    var height = 750;
 
     // Create SVG
     var svg = d3.select( ".seattle" )
@@ -51,19 +56,79 @@ class Map extends Component {
     .data( seattleJson.features )
     .enter()
     .append( "path" )
-    .attr( "fill", "#ccc" )
+    .attr( "fill", "transparent" )
     .attr( "stroke", "#333")
     .attr( "d", path );
+
+    // var coordinates = collection.features.map(feature => {
+    //   console.log(feature)
+    //   return feature.geometry.coordinates
+    // })
+    var coordinates = this.state.dots.features.map(feature => {
+      console.log(feature)
+      return feature.geometry.coordinates
+    })
+    console.log(coordinates)
+
+    svg.selectAll("circle")
+		.data(coordinates).enter()
+		.append("circle")
+    .attr("cx", function (d) { console.log(d); console.log(projection(d)); return projection(d)[0]; })
+    .attr("cy", function (d) { return projection(d)[1]; })
+		.attr("r", "2px")
+    .attr("fill", "#777a")
+    
+    this.getPoints()
+  }
+
+  geoJsonify(response_data) {
+    // console.log(response.data)
+    const time = response_data.last_updated
+
+    const jsonFeatures = response_data.data.bikes.map(bike => {
+      return {
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [bike.lon, bike.lat]
+        },
+        'properties': {
+          'time': time,
+          'bike_id': bike.bike_id,
+          'name': bike.name,
+          'is_reserved': bike.is_reserved,
+          'is_disabled': bike.is_disabled,
+          'jump_ebike_battery_level': bike.jump_ebike_battery_level,
+          'jump_vehicle_type': bike.jump_vehicle_type
+        }
+      };
+    });
+
+    const collection = {
+      'type': 'FeatureCollection',
+      'features': jsonFeatures
+    }
+
+    return collection
   }
   
+  getPoints() {
+    // commented out while in test mode
+    axios.get('https://sea.jumpbikes.com/opendata/free_bike_status.json')
+      .then(response => {
+        this.setState({dots: this.geoJsonify(response.data)})
+        console.log("updated state!")
+      })
+      .catch(response => {
+        console.log(response.errors)
+      })
+  }
+
 
   render() {
-
     return (
       <div className="map">
-        <h1>This is a map!</h1>
         <section className="seattle"></section>
-        {/* { console.log(seattleJson.features) } */}
       </div>
     );
   }
